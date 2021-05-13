9ffe027766c62c3fc83f5bcbdf13ae15817cfb62
Jony <jonyreiscardoso@gmail.com>
2021-05-13 11:10:21 -0400

    feat: add Intro component

---
+++ src/components/Intro/index.tsx
@@ -0,0 +1,13 @@
+import { IntroContainer } from "./styles";
+
+const Intro = () => {
+  return (
+    <IntroContainer>
+      <h1 className="the-geatest">The Geatest App</h1>
+      <span>for</span>
+      <h1 className="lottery">LOTTERY</h1>
+    </IntroContainer>
+  );
+};
+
+export default Intro;
---
+++ src/components/Intro/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const IntroContainer = styled.section`
+  color: #707070;
+  display: grid;
+  justify-items: center;
+  h1 {
+    font-style: italic;
+    font-weight: bold;
+    text-align: center;
+  }
+  .the-geatest {
+    font-size: 65px;
+    width: 240px;
+  }
+  .lottery {
+    font-size: 83px;
+  }
+  span {
+    background-color: #b5c401;
+    border-radius: 100px;
+    color: #fff;
+    font-weight: bold;
+    font-style: italic;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 144px;
+    height: 39px;
+    margin: 16px 0;
+  }
+`;
