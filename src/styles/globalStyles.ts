b267ff4922ac404be34e45b1c8287ff81d54b10e
Jony <jonyreiscardoso@gmail.com>
2021-05-13 09:09:22 -0400

    chore: add styled-components

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  html, body, div, span, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, a, em, img, dl, dt, dd, ol, ul, li, fieldset, form, label, input, legend, article, footer, header, nav, section, main {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+`;
