bc77a9f4dcd77a5d5cfb49e741e3fe52c4128876
Jony Reis <jonyreiscardoso@gmail.com>
2021-05-19 08:41:52 -0400

    feat: add logic of the public route and private route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useSelector, RootStateOrAny } from 'react-redux';
 
 import Login from "../pages/Login";
 import Registration from '../pages/Registration';
 import ResetPassword from "../pages/ResetPassword";
+import Home from "../pages/Home";
 
 
 const Routes: React.FC = () => {
+  const { auth } = useSelector((state: RootStateOrAny) => state)
+
+  const publicRoute = (Component: React.FC) =>
+  auth[0].name ? () => <Redirect to="/home" /> : Component
+
+  const privateRoute = (Component: React.FC) =>
+  auth[0].name ? Component : () => <Redirect to="/" />
 
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="/registration" exact component={Registration} />
-      <Route path="/reset-password" exact component={ResetPassword} />
+      <Route path="/" exact component={publicRoute(Login)} />
+      <Route path="/registration" exact component={publicRoute(Registration)} />
+      <Route path="/reset-password" exact component={publicRoute(ResetPassword)} />
+      <Route path="/home" exact component={privateRoute(Home)} />
     </Switch>
   );
 };
