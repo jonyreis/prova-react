0c9c6ca48285930a79e1d1a9f100626016fa0955
Jony Reis <jonyreiscardoso@gmail.com>
2021-05-26 15:34:40 -0400

    feat: add Bets component on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector, RootStateOrAny } from 'react-redux'
+
+import convertToCurrency from '../../utils/convertToCurrency';
+
 import Header from "../../components/Header"
 import GamesBtn from '../../components/GamesBtn';
 
 import ArrowRightGreen from "../../assets/arrow-right-green.svg";
 
-import { HomeContainer } from './styles'
+import { 
+  HomeContainer, 
+  Main, 
+  FiltersAndLink, 
+  FiltersContainer, 
+  Bets,
+  Bet,
+  Separator,
+  Numbers,
+  BetInfo,
+  DateAndPrice,
+  TypeBet,
+  NewBet
+ } from './styles'
 import { IGames } from '../../store/modules/games/types';
 
 const Home = () => {
@@ -21,6 +36,7 @@ const Home = () => {
   })
 
   const dispatch = useDispatch()
+  const { bets } = useSelector((state: RootStateOrAny) => state)
 
   async function getGame() {
     const response = await fetch('games.json')
@@ -51,18 +67,42 @@ const Home = () => {
   return (
     <HomeContainer>
       <Header />
-      <main>
-        <div className="filters-container">
-          <div>
+      <Main>
+        <FiltersAndLink>
+          <FiltersContainer>
             <h2>Recent games</h2>
-            <span>Filters: </span>
+            <span>Filters </span>
             <ul>
               <GamesBtn setSelectedGame={setSelectedGame} />
             </ul>
-            </div>
-          <Link className="btn-new-bet" to="/new-bet">New Bet <img src={ArrowRightGreen} alt="" /></Link>
-        </div>
-      </main>
+          </FiltersContainer>
+          <NewBet to="/new-bet">New Bet <img src={ArrowRightGreen} alt="" /></NewBet>
+        </FiltersAndLink>
+        <Bets>
+          {bets.map((bet: 
+            { 
+              numbers: Array<number>; 
+              color: string; 
+              date: string;
+              price: number; 
+              type: string; 
+              key: string 
+            }) => 
+            <Bet key={bet.key}>
+              <Separator color={bet.color} />
+              <BetInfo>
+                <Numbers>
+                  {bet.numbers.map((number: number) => 
+                    <h4 key={number}>{String(number).padStart(2, '0')}</h4>
+                  )}
+                </Numbers>
+                <DateAndPrice>{bet.date} - ({convertToCurrency(bet.price)})</DateAndPrice>
+                <TypeBet color={bet.color}>{bet.type}</TypeBet>
+              </BetInfo>
+            </Bet>  
+          )}
+        </Bets>
+      </Main>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,57 +1,159 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const HomeContainer = styled.div`
   background-color: #F7F7F7;
   height: calc(100vh - 79px);
+`
 
-  main {
-    max-width: 1040px;
-    margin: 0 auto;
-    padding-top: 80px;
+export const Main = styled.main`
+  max-width: 1040px;
+  margin: 0 auto;
+  padding-top: 80px;
+`
+
+export const FiltersAndLink = styled.section`
+  display: flex;
+  align-items: center;
+`
+
+export const FiltersContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 630px;
+
+  h2 {
+    color: #707070;
+    font-size: 24px;
+    font-style: italic;
+    text-transform: uppercase;
   }
 
-  .filters-container {
-    display: flex;
-    align-items: center;
-    div {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      min-width: 630px;
-
-      h2 {
-        color: #707070;
-        font-size: 24px;
-        font-style: italic;
-        text-transform: uppercase;
-      }
-
-      span {
-        color: #868686;
-        font-size: 17px;
-        font-style: italic;
-      }
-    }
+  span {
+    color: #868686;
+    font-size: 17px;
+    font-style: italic;
   }
-
   ul {
     display: flex;
   }
+`
 
-  .btn-new-bet {
-    color: #B5C401;
-    font-size: 24px;
-    font-style: italic;
-    font-weight: 700;
-    text-decoration: none;
+export const NewBet = styled(Link)`
+  color: #B5C401;
+  font-size: 24px;
+  font-style: italic;
+  font-weight: 700;
+  text-decoration: none;
 
+  display: flex;
+  align-items: center;
+
+  margin-left: auto;
+
+  img {
+    margin-left: 8px;
+  }
+`
+
+export const Bets = styled.ul`
+  display: flex;
+  flex-direction: column;
+  max-height: 500px;
+  padding: 16px 0;
+  margin: 16px 0;
+  overflow: auto;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #ddd;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #aaa;
+    border-radius: 8px;
+  }
+`
+
+export const Bet = styled.li`
+  display: flex;
+  margin: 16px 0;
+  align-items: center;
+  .bet-info {
     display: flex;
-    align-items: center;
-
-    margin-left: auto;
-
-    img {
-      margin-left: 8px;
+    flex-direction: column;
+    .numbers {
+      display: flex;
     }
   }
+`
+
+interface ISeparatorProps {
+  color: string
+}
+
+export const Separator = styled.div<ISeparatorProps>`
+  content: "";
+  border-radius: 100px;
+  background-color: ${props => props.color};
+  display: inline-block;
+
+  width: 6px;
+  height: 94px;
+  margin-right: 16px;
+`
+
+export const BetInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 90px;
+`
+
+export const Numbers = styled.div`
+  display: flex;
+  h4 {
+    color: #868686;
+
+    font-size: 20px;
+    font-style: italic;
+    font-weight: bold;
+    letter-spacing: 0px;
+
+    &::after {
+      content: ',';
+      display: inline-block;
+      
+      margin-right: 4px;
+      &:last-child {
+        display: none;
+      }
+    }
+    &:last-child {
+      &::after {
+      display: none;
+      }
+    }
+  }
+`
+
+export const DateAndPrice = styled.h3`
+  color: #868686;
+  font-size: 17px;
+  font-weight: 400;
+`
+
+interface ITypeBetProps {
+  color: string
+}
+
+export const TypeBet = styled.h2<ITypeBetProps>`
+  color: ${props => props.color};
+  font-size: 20px;
+  font-weight: 700;
+  font-style: italic;
 `
